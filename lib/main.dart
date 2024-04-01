1fc88c79f79695642d566eeffb7b603e8f268097
ilhamyual <82436226+ilhamyual@users.noreply.github.com>
2024-04-01 21:51:56 +0700

    splash ama landing udah dak tau bagus endak

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_projek/login/login_page.dart';
+import 'package:flutter_projek/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: login_page(),
+      home: SplashScreen(),
     );
   }
 }
